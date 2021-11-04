9fd9ba3ee73e75f62c6d803e374babb51e0914a7
hjalban <hjalban@espol.edu.ec>
2021-11-04 12:38:43 -0500

    Se agrega CalendarActivity

--- app/src/main/java/com/example/amst2/MainActivity.java
+++ app/src/main/java/com/example/amst2/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.amst2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnMapa;
+    Button btnVideo;
+    Button btnCalen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMapa = (Button) findViewById(R.id.btnMapa);
+        btnVideo = (Button) findViewById(R.id.btnVideo);
+        btnCalen= (Button) findViewById(R.id.btnCalen);
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCalen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplicationContext(),CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
